b1dfd60f36f93b2536c8f5a0641d752042ef41c4
mazdeh <vahid.mazdeh@gmail.com>
2015-02-09 16:34:48 -0700

    fixed #6

--- routes/contextView.js
+++ routes/contextView.js
@@ -2,7 +2,8 @@ module.exports = function(app) {
 
     app.get('/context/view/:id', function(req, res) {
 
-        var context = app.data.contexts[31]
+        var id = req.params.id
+        var context = app.data.contexts[id]
 
         res.render('contextView.jade', {
             context: context
