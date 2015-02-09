455a94201fd61cc4a165bc778a0d104f0120501a
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-02-09 15:59:10 -0700

    Finished issue #1 and issue #4, and working on more basic info on the note view (issue 2)

--- routes/noteList.js
+++ routes/noteList.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 module.exports = function(app) {
 
     app.get('/note/list', function(req, res) {
@@ -6,10 +8,14 @@ module.exports = function(app) {
         var page = req.query.page || 1
 
         // TODO: calculate the right set of notes based on 'perPage' and 'page'
-        var notes = app.data.notes.slice(0,20)
+        var notes = _.filter(app.data.notes, function(n) {
+            return n.medias && n.medias.length > 0
+        })
+        var length = notes.length
+        notes = notes.slice((page - 1) * perPage, page * perPage)
 
         // TODO: calculate the correct array of page numbers for rendering the paginator
-        var pageNumbers = [1, 2, 3, 4, 5]
+        var pageNumbers = _.range(1, length/perPage + 1)
 
         res.render('noteList.jade', {
             notes: notes,
