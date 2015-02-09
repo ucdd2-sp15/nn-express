0d4eeb178d8372e9c611e91c5cfc2891b574a2c9
Peyman Mortazavi <pmortazavi@cailmobility.com>
2015-02-09 16:30:13 -0700

    fixed issue #8

--- routes/contextList.js
+++ routes/contextList.js
@@ -4,9 +4,19 @@ module.exports = function(app) {
 
         var contexts = app.data.contexts
 
+        var model = {};
+
+        contexts.forEach(function(item){
+
+        	if(!model[item.site.description])
+        		model[item.site.description] = [];
+
+        	model[item.site.description].push(item);
+
+        });
+
         res.render('contextList.jade', {
-            contexts: contexts
+            contexts: model
         })
     })
-
 }
