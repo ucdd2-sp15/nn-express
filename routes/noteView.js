9b979b8431141d118a7b8261d76488eae1276600
rshamirza <rshamirza@rgnt2-73-137-dhcp.int.colorado.edu>
2015-02-09 16:33:26 -0700

    fixed issue #3

--- routes/noteView.js
+++ routes/noteView.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
-
+	var _ = require('lodash')
     app.get('/note/view/:id', function(req, res) {
 
         // TODO: get the correct note by id
-        var note = app.data.notes[17]
+        var note = app.data.notes[req.params.id]
 
         res.render('noteView.jade', {
             note: note
