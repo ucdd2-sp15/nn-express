869d4503cf58f80013408ef1b88535b6e0cf483c
Nir Boneh <nboneh5921@gmail.com>
2015-02-09 16:24:03 -0700

    fixed #5

--- routes/accountView.js
+++ routes/accountView.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
 
     app.get('/account/view/:id', function(req, res) {
 
-        var account = app.data.accounts[req.params.id];
+        var account = app.data.accounts[req.params.id -1];
 
         res.render('accountView.jade', {
             account: account
