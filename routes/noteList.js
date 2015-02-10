b4f8e1ab8b021576fd0361a97dd13286ac5599fe
Adrian Chen <adrianchen@rgnt2-123-19-dhcp.int.colorado.edu>
2015-02-09 17:03:11 -0700

    Fixed merge conflicts

--- routes/noteList.js
+++ routes/noteList.js
@@ -8,17 +8,13 @@ module.exports = function(app) {
         var page = req.query.page || 1
 
         // TODO: calculate the right set of notes based on 'perPage' and 'page'
-<<<<<<< HEAD
-        var notes = _.filter(app.data.notes, function(x) { return x.medias.length > 0; }).slice(0,20);
-=======
-        var notes = app.data.notes.slice(0,perPage);
+        var notes = _.filter(app.data.notes, function(x) { return x.medias.length > 0; }).slice(0,perPage);
         
         if (page != 1) {
             var newStart = perPage * (page - 1);
             var newEnd = page * perPage;
             notes = app.data.notes.slice(newStart,newEnd);
         }
->>>>>>> bb3c7aa413d705bead1ecbd4144a537001f61aee
 
         // TODO: calculate the correct array of page numbers for rendering the paginator
         var pageNumbers = [1, 2, 3, 4, 5]
