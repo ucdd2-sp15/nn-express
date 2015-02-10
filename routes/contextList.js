5a4d2f41c9f5a7895cc359fa2f362f4377e20fc3
BrianNewsom <brian.newsom@colorado.edu>
2015-02-09 17:12:12 -0700

    Fix Issue #8

--- routes/contextList.js
+++ routes/contextList.js
@@ -4,8 +4,20 @@ module.exports = function(app) {
 
         var contexts = app.data.contexts
 
+        var reOrg = {};
+        // console.log(contexts[0].site.description)
+
+        // Get all sites
+        for (var i = 0 ; i < contexts.length ; i++){
+            var site = contexts[i].site.description;
+            if(!reOrg[site]){
+                reOrg[site] = [];
+            }
+            reOrg[site].push(contexts[i])
+        }
+
         res.render('contextList.jade', {
-            contexts: contexts
+            contexts: reOrg
         })
     })
 
