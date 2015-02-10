f16388a6a9e52da3edc6786b5dd99e50c32dac5b
wuzhi1023 <woozhh@gmail.com>
2015-02-09 17:25:34 -0700

    issue 7

--- routes/accountView.js
+++ routes/accountView.js
@@ -1,11 +1,19 @@
 module.exports = function(app) {
 
     app.get('/account/view/:id', function(req, res) {
-
+    	var id = req.params.id
         var account = app.data.accounts[5]
-
+        var notes = app.data.notes.filter(function(item){
+            if(item.account){
+            	if(item.account.id){
+            		if(item.account.id == id) return item
+            	}
+            }
+        })
+        console.log(notes)
         res.render('accountView.jade', {
-            account: account
+            account: account,
+            notes: notes
         })
     })
 
