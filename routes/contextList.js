2b234f90869f9a459dc19ba0641089d9c626449d
mazdeh <vahid.mazdeh@gmail.com>
2015-02-09 18:23:31 -0700

    fixed #8

--- routes/contextList.js
+++ routes/contextList.js
@@ -1,11 +1,19 @@
+var _ = require('lodash')
 module.exports = function(app) {
 
     app.get('/context/list', function(req, res) {
 
         var contexts = app.data.contexts
+        var sites = []
+         for (i = 0; i < contexts.length; i++) {
+         	if (!(_.includes(sites, contexts[i].site.description))) {
+         		sites.push(contexts[i].site.description)
+         	}
+         }
 
         res.render('contextList.jade', {
-            contexts: contexts
+            contexts: contexts,
+            sites: sites
         })
     })
 
