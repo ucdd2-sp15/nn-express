bcd5576d2f3a3145e1b94df3e636a5e7f7455272
dawsonbotsford <dawsonbotsford@gmail.com>
2015-02-11 20:37:49 -0700

    Better template code for y'all

--- mongo/contextList.js
+++ mongo/contextList.js
@@ -1,5 +1,24 @@
 module.exports = function(app) {
 
-    // TODO
-    
+    app.get('/account/list', function(req, res) {
+
+        // get the business collection
+        var accounts = app.db.get('accounts')
+
+        // compose a query to look up docs whose 'categories' field contains the word 'Doctors'
+        var q = {
+            '_model_': {
+                $in: ['Account']
+            }
+        }
+
+        // execute the query to find those matched limiting to 20
+        accounts.find(q, {
+            limit: 20
+        }, function(err, accounts) {
+            res.render('accountList.jade', {
+                accounts: accounts,
+          })
+        })
+    })
 }
