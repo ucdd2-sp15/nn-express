2d81d1624204aefe128fbf42f217cea7f763dc6b
Hay-Z <hberge17@gmail.com>
2015-02-12 10:34:25 -0700

    Implemented Contexts List
    Implemented Contexts List

---
+++ mongo/contextsList.js
@@ -0,0 +1,26 @@
+module.exports = function(app) {
+
+	app.get('/list/contexts', function(req, res) {    //
+
+        // get the business collection
+        var contexts = app.db.get('contexts') //tells to get contexts collection
+
+      
+        //var q = {
+           // '_models_': {
+             //   $in: ['Contexts']
+           // }
+        //}
+
+        // execute the query to find those matched limiting to 20
+        contexts.find({}, {
+            limit: 20
+        }, function(err, contexts) {
+
+            res.render('contextList.jade', {
+                contexts: contexts
+            })
+        })
+
+    })
+}
