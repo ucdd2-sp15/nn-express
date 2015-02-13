be1c73589eb22e8a96033016c6e13f58f073d3bf
Abebe <abta3355@colorado.edu>
2015-02-13 09:50:11 -0700

    small bug

--- mongo/contextView.js
+++ mongo/contextView.js
@@ -3,16 +3,14 @@ module.exports = function(app) {
     app.get('/context/view/:id', function(req, res) {
 
         // get the business collection
-        var contextsDb = app.db.get('contexts')
+        var context = app.db.get('contexts')
 
          var q = {
-            'id': req.params.id            
+            "id": req.params.id            
         }
-
-        var context = contextsDb.findOne(q, function(err, item) {
-
+        var context = context.findOne(q, function(err, item) {
             res.render('contextView.jade', {
-                context: item
+                context:item
             })
         })
 
