12813c567c2e3caa1af801b509016f32e6f11298
Samuel Tobey <Samuel.P.Tobey@colorado.edu>
2015-02-13 11:33:32 -0700

    Context list sorted by description

--- mongo/contextList.js
+++ mongo/contextList.js
@@ -3,19 +3,25 @@ module.exports = function(app) {
     app.get('/context/list', function(req, res) {
 
         // get the context collection
-        var contexts = app.db.get('contexts');
+        var contexts = app.db.get('contexts')
 
         // execute the query to find those matched
         contexts.find(
             {'_model_': { $in: ["Context"] }},
             { },        //{ limit: 20 }, 
             function(err, items) {
-            res.render('contextList.jade', {
-                contexts: {
-                    items: items
-                    //description: description
-                }
-            })
-        })
+                var descriptions = {};
+                items.forEach(function(item) {
+                    if( !descriptions[item.site.description] ) {
+                        descriptions[item.site.description] = [];
+                    }
+                    descriptions[item.site.description].push(item);
+                });
+                //console.log(descriptions);
+                res.render('contextList.jade', {
+                    contexts: descriptions
+                })
+            }
+        )
     })
 }
