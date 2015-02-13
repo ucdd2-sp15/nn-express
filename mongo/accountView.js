b5f38a7aa2388469a4d7679fbd6b2a689a81e0f2
melissab147 <melissa.j.bica@gmail.com>
2015-02-13 11:45:44 -0700

    account view done

--- mongo/accountView.js
+++ mongo/accountView.js
@@ -1,3 +1,29 @@
 module.exports = function(app){
 
+    app.get('/account/view/:id', function(req, res) {
+
+        // get the accounts collection
+        var accounts = app.db.get('accounts')
+		var notes = app.db.get('notes')
+
+		//query to get account with matching id
+        var q = {
+            'id': Number(req.params.id)
+        }
+
+        var account = accounts.findOne(q, function(err, account) {
+        	
+        	//query to get notes whose author (username) matches the current account username
+        	var q2 = {
+        		'account.username': account.username
+        	}
+
+        	var note = notes.find(q2, function(err, notes){
+	            res.render('accountView.jade', {
+	                account: account,
+	                notes: notes
+	            })        		
+        	})
+        })
+    })
 }
