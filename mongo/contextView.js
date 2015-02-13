552113916fb927396e79b3e1b82d44b61e0f8bcd
Abebe <abta3355@colorado.edu>
2015-02-13 15:14:33 -0700

    conextView

--- mongo/contextView.js
+++ mongo/contextView.js
@@ -3,14 +3,15 @@ module.exports = function(app) {
     app.get('/context/view/:id', function(req, res) {
 
         // get the business collection
-        var context = app.db.get('contexts')
+        var contexts = app.db.get('contexts')
 
          var q = {
-            "id": req.params.id            
+            "id":Number(req.params.id)           
         }
-        var context = context.findOne(q, function(err, item) {
+		console.log(q)
+        contexts.findOne(q, function(err, context) {
             res.render('contextView.jade', {
-                context:item
+                context:context
             })
         })
 
