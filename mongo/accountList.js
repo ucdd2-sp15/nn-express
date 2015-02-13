d7d911417028d64f0be662b254c78f5118f9b3aa
melissab147 <melissa.j.bica@gmail.com>
2015-02-13 10:58:29 -0700

    account list done

--- mongo/accountList.js
+++ mongo/accountList.js
@@ -1,3 +1,21 @@
 module.exports = function(app){
+    app.get('/account/list', function(req, res) {
 
+        // get the accounts collection
+        var accounts = app.db.get('accounts')
+
+        // compose a query to look up accounts whose '_model_' field contains the word 'Account'
+        var q = {
+            '_model_': 'Account'
+        }
+
+        // execute the query
+        accounts.find(q, function(err, accts) {
+
+            res.render('accountList.jade', {
+                accounts: accts
+            })
+        })
+
+    })
 }
