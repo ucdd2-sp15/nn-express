c9ab02037a5bf637bf788ff0b6fa2b3cafb447d1
melissab147 <melissa.j.bica@gmail.com>
2015-02-13 12:14:24 -0700

    update port

--- mongo.js
+++ mongo.js
@@ -27,7 +27,7 @@ require('./mongo/accountList')(app)
 require('./mongo/accountView')(app)
 
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
 
     var host = server.address().address
     var port = server.address().port
